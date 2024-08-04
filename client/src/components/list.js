159e79d8186003e7f7701a152214245afe8bb505
NISHCHAY12 <nishchaysingh13579@gmail.com>
2024-08-04 19:40:59 +0530

    Added lists for displaying tasks

--- client/src/components/list.js
+++ client/src/components/list.js
@@ -1,10 +1,25 @@
+import { Collapse,Panel } from "antd";
+
+const items: CollapseProps['items'] = [
+  {
+    key: '1',
+    label: 'This is panel header 1',
+    children: <p>hello</p>,
+  },
+  {
+    key: '2',
+    label: 'This is panel header 2',
+    children: <p>hello</p>,
+  },
+];
+
 function List() {
-    return (
-      <div className="mx-auto p-8 h-auto w-1/2 bg-blue-50 shadow-lg my-8 rounded">
-      
-        
-      </div>
-    );
-  }
-  
-  export default List;
+  return (
+    <div className="mx-auto p-8 h-auto w-1/2 bg-blue-50 shadow-lg my-8 rounded">
+
+      <Collapse items={items} defaultActiveKey={['1']} />
+    </div>
+  );
+}
+
+export default List;
